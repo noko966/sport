85725277dcbeccac8f019c6d19b77b4f958f1cb9
noko966 <kananovn@gmail.com>
2019-11-04 18:36:26 +0400

    chat done

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -1,12 +1,21 @@
 import React from "react";
 
-export default function Chat({}) {
+export default function Chat({ messages }) {
   return (
     <div className="btcl_chat">
       <div className="btcl_chat_head"></div>
       <div className="btcl_chat_content">
-        <div className="btcl_chat_header"></div>
-        <div className="btcl_chat_messages"></div>
+        <div className="btcl_chat_header">
+          <i className="btcl_close">✖</i>
+        </div>
+        <div className="btcl_chat_messages">
+          <div className="btcl_chat_message btcl_chat_message_incoming">
+            some long text
+          </div>
+          <div className="btcl_chat_message btcl_chat_message_outgoing">
+            some long text
+          </div>
+        </div>
         <div className="btcl_chat_footer"></div>
       </div>
     </div>
